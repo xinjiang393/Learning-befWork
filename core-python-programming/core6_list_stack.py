a827e019db9caf8b283a1796832f771c16882475
xinjiang393 <385832410@qq.com>
2017-01-17 23:27:50 +0800

    Create core6_list_stack.py

---
+++ core-python-programming/core6_list_stack.py
@@ -0,0 +1,9 @@
+"""
+This simple scipt uses lists as a stack to store and retrive strings entered through
+this menu-driven text application using only the append() and pop() list methods.
+"""
+
+stack = [] #Defining lists as a stack to store strings.
+
+def pushit():
+  """"""

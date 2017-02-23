9c23df0eca884ffdb2074036bbde659e9be0b6e6
xinjiang393 <385832410@qq.com>
2017-02-23 17:59:17 +0800

    Update chp11_gloloc_scope.py

--- core-python-programming/chp11_gloloc_scope.py
+++ core-python-programming/chp11_gloloc_scope.py
@@ -29,3 +29,36 @@ def foo():
 >>>foo()
 7
 This is global scope of foo() within bar():3
+	
+"""
+If references are made from inside an inner function to an object defined in any outer scopre
+(but not in the global scope),the inner function then is known as a closure.The variables defined 
+in the outer function but used or referenced to by the inner function are called free variable
+"""
+
+def counter(start_at = 0):
+	count = [start_at]
+	def incr():
+		count[0] += 1
+		return count[0]
+	return incr
+>>>co = couter(5)
+>>>co()
+6
+
+def counter_new(start_at = 0):
+	count = start_at
+	print(count)
+	def incr():
+		count += 1
+		return count
+	return incr
+>>>cou = couter_new(5)
+5
+"""
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 5, in incr
+UnboundLocalError: local variable 'count' referenced before assignment
+cout的类型是int和list有什么不同？？
+"""

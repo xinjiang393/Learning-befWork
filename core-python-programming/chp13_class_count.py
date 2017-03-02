b78730c7c62357966dca89d5f33106675596d45c
xinjiang393 <385832410@qq.com>
2017-03-02 17:05:06 +0800

    Update chp13_class_count.py

--- core-python-programming/chp13_class_count.py
+++ core-python-programming/chp13_class_count.py
@@ -19,3 +19,32 @@ class InstCt(object):
 >>>del a
 >>>InstCt.count
 0
+
+
+#Instance attributes VS class attributes
+#for immutable attributes
+class Foo(object):
+	x = 1.5	
+>>>foo = Foo()
+>>>foo.x = 1.7
+>>>foo.x
+1.7
+>>>Foo.x
+1.5
+>>>del foo.x
+>>>foo.x
+1.5
+
+#for mutable attributes
+class Bar(object):
+	x = {2003:"poe2"}	
+>>>bar = Bar()
+>>>bar.x
+{2003:"poe2"}
+>>>bar.x[2004] = "valid path"
+>>>bar.x
+{2003:"poe2", 2004:"valid path"}
+>>>Bar.x
+{2003:"poe2", 2004:"valid path"}
+>>>id(bar.x) == id(Bar.x)
+True
